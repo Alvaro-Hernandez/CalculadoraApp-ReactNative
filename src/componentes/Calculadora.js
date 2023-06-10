7ec912f42ee86b78a4b9c109ed9298d95dea468a
Alvaro-Hernandez <alvaropineda606@gmail.com>
2023-06-09 20:48:15 -0600

    Creacion y Exportacion del Componente Calculadora

--- src/componentes/Calculadora.js
+++ src/componentes/Calculadora.js
@@ -3,3 +3,7 @@ import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
 import {create, all} from 'mathjs';
 
 const math = create(all);
+
+const Calculadora = () => {};
+
+export default Calculadora;
